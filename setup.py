cb1ccabdb8965801aae5f8983043bf3b7a0668a1
Avram Lubkin <aviso@rockhopper.net>
2019-08-26 11:38:48 -0400

    Remove pypy in setup for now

--- setup.py
+++ setup.py
@@ -58,8 +58,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Utilities',
+         'Topic :: Utilities',
     ],
     keywords='terminal console blessed curses',
 )
