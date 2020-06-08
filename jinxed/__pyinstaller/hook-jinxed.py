3c52b2487f32798701b0dadb021b840f5ee751bd
Legorooj <50370070+Legorooj@users.noreply.github.com>
2020-06-08 17:15:47 +1000

    Create the jinxed hook

---
+++ jinxed/__pyinstaller/hook-jinxed.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Avram Lubkin, All Rights Reserved
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# The PyInstaller compatibility module inside jinxed was originally authored by @Legorooj
+
+hiddenimports = [
+    'jinxed.terminfo.ansicon', 'jinxed.terminfo.vtwin10'
+]
