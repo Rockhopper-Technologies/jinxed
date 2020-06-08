b44b19fee92b90d288b3522837e2335ba9618977
Legorooj <50370070+Legorooj@users.noreply.github.com>
2020-06-08 17:16:51 +1000

    PyInstaller tests part one

---
+++ jinxed/__pyinstaller/conftest.py
@@ -0,0 +1,10 @@
+
+# -*- coding: utf-8 -*-
+# Copyright 2019 Avram Lubkin, All Rights Reserved
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# The PyInstaller compatibility module inside jinxed was originally authored by @Legorooj
+from PyInstaller.utils.conftest import *
