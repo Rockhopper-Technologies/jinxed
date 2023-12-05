f63e0c27e903ff1055640d90eb87cc247e611b99
Avram Lubkin <aviso@rockhopper.net>
2023-12-05 09:10:35 -0500

    Remove static path for docs

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Jinxed'
-copyright = '2019 - 2021, Avram Lubkin'
+copyright = '2019 - 2023, Avram Lubkin'
 author = 'Avram Lubkin'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -108,7 +108,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
