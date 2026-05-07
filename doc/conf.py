29d6c00b84486294d4374b205e766d1b65286c3f
Jeff Quast <contact@jeffquast.com>
2026-05-07 09:21:29 -0400

    move github_url to html_theme_options

--- doc/conf.py
+++ doc/conf.py
@@ -105,7 +105,9 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+        'github_url': 'https://github.com/Rockhopper-Technologies/jinxed'
+        }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
